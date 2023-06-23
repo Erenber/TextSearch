f51f442f9cc0dd3bea82f3a597fc1e2d9606c2dd
Erenber <dmtr2705@gmail.com>
2023-06-23 19:09:49 +0300

    full fixes

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#cd Alembic
-#alembic revision --autogenerate -m "DB creation"
-#alembic upgrade head
-#cd ..
+cd Alembic
+alembic revision --autogenerate -m "DB creation"
+alembic upgrade head
+cd ..
 uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
