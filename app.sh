e82f33143724aa420b81a69e8da3b3345c1beeb5
Erenber <dmtr2705@gmail.com>
2023-06-25 16:57:09 +0300

    Improved script, changed environment

--- app.sh
+++ app.sh
@@ -9,8 +9,23 @@ if [ "$DATABASE" = "postgres" ]; then
 
     echo "PostgreSQL started"
 fi
-
 cd Alembic
 alembic upgrade head
 cd ..
+
+
+if [ "$ES_DATABASE" = "elasticsearch" ]; then
+    echo "Waiting for Kibana..."
+
+    while ! curl -s $KIBANA_HOST:$KIBANA_PORT >/dev/null; do
+        sleep 1
+    done
+
+    echo "Kibana started"
+fi
+
+cd app/setup
+python elastic_search.py
+cd ../..
+
 uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
--- app/setup/elastic_search.py
+++ app/setup/elastic_search.py
@@ -1,5 +1,8 @@
 from elasticsearch import Elasticsearch
 
+from config import ES_HOST
+
+
 MAPPING = {
             "properties": {
                 "iD": {
@@ -13,7 +16,8 @@ MAPPING = {
 
 
 def create_indexes():
-    elastic_client: Elasticsearch = Elasticsearch("http://elastic:9200")    elastic_client.indices.create(index="documents", mappings=MAPPING)
+    elastic_client: Elasticsearch = Elasticsearch(f"http://{ES_HOST}:9200")
+    elastic_client.indices.create(index="documents", mappings=MAPPING)
 
 
 if __name__ == "__main__":
